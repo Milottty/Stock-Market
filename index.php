5fec3c7d92eb4091a009fa323d04fe4234c1f25b
Milottty <milotymeri1@gmail.com>
2025-05-20 22:12:11 +0200

    day 1

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include_once "config.php";
+include_once "header.php";
+
+?>
+
+<div class="container-fluid">
+  <div class="row">
+    <!-- Sidebar -->
+    <nav class="col-md-2 d-md-block bg-light sidebar">
+      <!-- Sidebar content here -->
+    </nav>
+
+    <!-- Main Content -->
+    <main class="col-md-10 ms-sm-auto px-md-4">
+      <!-- Navbar -->
+      <nav class="navbar navbar-light bg-white">
+        <!-- Search & User Info -->
+      </nav>
+
+      <!-- Portfolio Section -->
+      <div class="row my-4">
+        <!-- Portfolio cards (Apple, Meta, etc.) -->
+      </div>
+
+      <!-- Graph & Watchlist Section -->
+      <div class="row">
+        <div class="col-md-8">
+          <!-- Graph/Chart Area -->
+        </div>
+        <div class="col-md-4">
+          <!-- Watchlist -->
+        </div>
+      </div>
+    </main>
+  </div>
+</div>
+
+<?php
+include_once "footer.php";
+
+?>
